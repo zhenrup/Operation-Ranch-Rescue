d700f7639f78ae52e92d898a6314c89681e8766e
Omar Tahir <otahir3@uwo.ca>
2023-04-01 18:37:02 -0400

    player death/respawn logic added, and a trap

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rigidbody;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //checks the object 'tag' that is passed when the player collides with an object
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        //first, lets switch to the death animation, and make player movement static
+        rigidbody.bodyType = RigidbodyType2D.Static;
+        animator.SetTrigger("death");
+    }
+
+    private void RestartLevel()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+
+}
